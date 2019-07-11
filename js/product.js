393c72018c356fb8ba62d103e203833071df1c45
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-07-11 13:08:01 -0300

    deleted code in the product.js

--- js/product.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    
-    $('.menu-main').click(function) {
-        $().$('.container-prod').css('display:''none');
-        $('.container-prod').toggle(1000);
-    }
-});
