617e9e80bda84421f760b11cdde788b837d1bf77
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-07-12 11:42:34 -0300

    modifying product.js

--- js/product.js
+++ js/product.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     
     $('#entradas').click(function(){
       $('#container').Toggle('slow');
+      $('.menu-main').removeClass('first');   
+      $('#entradas').addClass('first');
     });
 
     $('#combinados').click(function(){
