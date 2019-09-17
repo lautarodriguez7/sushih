80239d4b2931dbcb9e9ddd360d87e975f1590066
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-09-17 18:29:37 -0300

    new function added in login.js [onclick]

--- js/login.js
+++ js/login.js
@@ -3,5 +3,11 @@ function funcion() {
     var input2 = document.getElementById('w-email');
     input.style.padding = "5px";
     input2.style.padding = "10px";
+}
+function funcion2() {
+    var input = document.getElementById('w-password');
+    var input2 = document.getElementById('w-email');
+    input.style.padding = "10px";
+    input2.style.padding = "5px";
 
 }
