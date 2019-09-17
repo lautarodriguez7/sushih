5db7523d32292d9a0429e29d9d402655eb912b54
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-09-17 18:03:56 -0300

    added code login.js

--- js/login.js
+++ js/login.js
@@ -2,4 +2,8 @@ $(document).ready(function(){
     $("input[id='w-email']").click(function(){
         $('#w-email').css('padding','55px');
     });
+
+    $("input[id='w-password']").click(function(){
+        $('#w-password').css('padding','55px');
+    });
 });
