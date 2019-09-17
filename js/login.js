f302974f43977cba5580d1a4a4c4da4827cbb877
Lautaro Rodriguez <lautarodriguez17@gmail.com>
2019-09-17 18:32:11 -0300

    code modified in .container-input "login.css"

--- js/login.js
+++ js/login.js
@@ -1,13 +1,13 @@
 function funcion() {
     var input = document.getElementById('w-password');
     var input2 = document.getElementById('w-email');
-    input.style.padding = "5px";
+    input.style.padding = "2px";
     input2.style.padding = "10px";
 }
 function funcion2() {
     var input = document.getElementById('w-password');
     var input2 = document.getElementById('w-email');
     input.style.padding = "10px";
-    input2.style.padding = "5px";
+    input2.style.padding = "2px";
 
 }
